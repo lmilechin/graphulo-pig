9c1aed09baa50e336e4ef2c0384074a9041a71f7
Tim Weale <timothy.weale@ll.mit.edu>
2016-07-27 15:19:28 -0400

    Added directory for Pig code and configurations. Works based on initial InsertAdjacencyGraph code.

--- src/edu/mit/ll/graphulo/pig/backend/InsertAdjacencyGraph.java
+++ src/edu/mit/ll/graphulo/pig/backend/InsertAdjacencyGraph.java
@@ -5,54 +5,55 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
+import java.util.Properties;
 
-import org.apache.hadoop.io.Text;
 import org.apache.log4j.Logger;
 import org.apache.pig.EvalFunc;
-import org.apache.pig.backend.hadoop.accumulo.AccumuloStorage;
-import org.apache.pig.data.DataByteArray;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.impl.util.WrappedIOException;
 
-import edu.mit.ll.graphulo.Graphulo;
 import edu.mit.ll.graphulo.pig.backend.LocalFileUtil;
 import edu.mit.ll.graphulo.util.TripleFileWriter;
 
 
 /**
- * A class to execute Breadth-First Search on Graphulo graphs.
- * 
- *
+ * A class to create a Graphulo graph in Accumulo.
+ * <p>
+ * File is expected to have the form: 
+ *	
  */
 public class InsertAdjacencyGraph extends EvalFunc<String> {
 
     /**
-     * Executes the BFS command, given the input parameters.
+     * Inserts an Adjacency graph into Accumulo.
      * 
-     * @param AccConfigFile			Name of file containing Accumulo configuration information
-     * @param GraphTable 		    Name of Accumulo table containing the graph to be searched.
-     * @param v0          			Starting nodes, like "a,f,b,c,". Null or empty string "" means start from all nodes.
-     *                    			v0 may be a range of nodes like "c,:,e,g,k,:,".
-     * @param k           			Number of steps
-     * @param ResultTable			Name of table to store result. Null means don't store the result.
-     * @param BFSConfigFile			Name of file containing additional BFS configuration information. 
+     * @param input Tuple containing three or four pieces of information required to insert data into Graphulo
+     * 
+     * <ol>
+     * <li> <b>AccumuloConfiguration:</b> Name of the file containing Accumulo configuration information
+     * <li> <b>inputFile:</b> File that contains the graph data
+     * <li> <b>graphName:</b> Name of the graph as it will be stored in Graphulo
+     * <li> <b>insertConfiguration:</b> Name of the file containing any additional insertion configuration information
+     * </ol>
      */
     public String exec(Tuple input) throws IOException {
         
     	try {
     		
     		// Sanity Check
-	    	if (input.size() == 0 || input.size() == 0)
+	    	if (input.size() < 3 || input.size() > 4)
 	            return null;
 	    	
 	    	// TableMult configuration information
             String accumuloConfiguration 	= (String) input.get(0);
             String inputFile 				= (String) input.get(1);
             String graphName 				= (String) input.get(2);
-            String insertConfiguration		= (String) input.get(3);
-
-            Map<String,Object> configuration = LocalFileUtil.parseConfiguration(insertConfiguration);
+            String insertConfiguration 		= null;
+            
+            if(input.size() == 4) {
+	            insertConfiguration		= (String) input.get(3);
+	            Properties configuration = LocalFileUtil.parseConfiguration(insertConfiguration);
+            }
             
     		BufferedReader br = new BufferedReader(new FileReader(inputFile));
     		PrintWriter pwV = new PrintWriter("verts.tmp");
@@ -85,7 +86,7 @@ public class InsertAdjacencyGraph extends EvalFunc<String> {
         	File edges = new File("edges.tmp");
         	File vals = null;
     		
-	    	TripleFileWriter tfw = new TripleFileWriter(LocalFileUtil.createConnection(accumuloConfiguration));
+	    	TripleFileWriter tfw = new TripleFileWriter(LocalFileUtil.createAccumuloConnection(accumuloConfiguration));
 	    	long l = tfw.writeTripleFile_Adjacency(verts, edges, vals, ",", graphName, true, false);
 	    	
 	    	return Long.toString(l);
